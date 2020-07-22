32bd5d73c1abc69f902a9914245f285ac4f2c72e
ecekic <64327424+ecekic@users.noreply.github.com>
2020-07-22 12:20:39 -0400

    Add files via upload

---
+++ PyDa.py
@@ -0,0 +1,38 @@
+'''
+go to wolfram alpha api page
+copy this stuff
+'''
+import wolframalpha
+
+client = wolframalpha.Client("JJEL8R-WYHXEX2EQL")
+
+import wikipedia
+
+'''
+want this to pop up on a window 
+so look up "python gui"
+pip install pysimplegui
+'''
+import PySimpleGUI as sg
+
+sg.theme('LightPurple')
+# All the stuff inside your window.
+layout = [[sg.Text('Enter a command'), sg.InputText()],
+          [sg.Button('Ok'), sg.Button('Cancel')]]
+
+# Create the Window
+window = sg.Window('Your AI', layout)
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+        break
+    # res = client.query(values[0])  # holds the question the user asks from wolfram
+    # wolfram_res = next(res.results).text
+
+    res = client.query(values[0])  # holds the question the user asks from wolfram
+    wolfram_res = next(res.results).text
+    wiki_res = wikipedia.summary(values[0], sentences=2)
+    sg.Popup(values[0], "Wolfram Result: " + wolfram_res,
+             "Wikipedia Result: " + wiki_res)  # opens answer in another window
+
+window.close()
